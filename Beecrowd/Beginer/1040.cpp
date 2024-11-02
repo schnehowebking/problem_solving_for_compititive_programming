d7f52e039fee4cb84d178cb265d7074f50d079a6
schnehowebking <schnehowebking@gmail.com>
2024-11-02 22:36:07 +0600

    added

--- Beecrowd/Beginer/1040.cpp
+++ Beecrowd/Beginer/1040.cpp
@@ -1,32 +1,37 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
+int main()
+{
+    float N1, N2, N3, N4, examScore;
+    cin >> N1 >> N2 >> N3 >> N4;
 
-        int m = (n + 1) / 2; 
+    // Calculate the weighted average
+    float average = (N1 * 2 + N2 * 3 + N3 * 4 + N4 * 1) / 10.0;
 
-        if (k > m) {
-            cout << -1 << endl;
-            continue;
+    cout << fixed << setprecision(1);
+    cout << "Media: " << average << endl;
+
+    if (average >= 7.0) {
+        cout << "Aluno aprovado." << endl;
+    } else if (average < 5.0) {
+        cout << "Aluno reprovado." << endl;
+    } else {
+        // Student is in exam
+        cout << "Aluno em exame." << endl;
+        cin >> examScore;
+        cout << "Nota do exame: " << examScore << endl;
+
+        // Recalculate the average with exam score
+        float finalAverage = (average + examScore) / 2.0;
+        
+        if (finalAverage >= 5.0) {
+            cout << "Aluno aprovado." << endl;
+        } else {
+            cout << "Aluno reprovado." << endl;
         }
-
-        vector<int> left_indices;
-        for (int i = 1; i <= n; i += 2) {
-            left_indices.push_back(i);
-            if (left_indices.size() == m) break;
-        }
-
-        cout << left_indices.size() << endl;
-        for (int idx : left_indices) {
-            cout << idx << " ";
-        }
-        cout << endl;
+        cout << "Media final: " << finalAverage << endl;
     }
+
     return 0;
 }
